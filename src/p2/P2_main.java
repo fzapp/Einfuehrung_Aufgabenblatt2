de81629e49bee9251f230aaee9f43bdc8a5866c6
fzapp <fzapp@smail.uni-loeln.de>
2025-10-29 12:40:00 +0100

    aenderung_Einfuehrung_Aufgabenblatt2_Aufg3.1

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,5 +1,14 @@
 package p2;
 
 public class P2_main {
+	
+	public static void main(String[] args) {
+		int i = 23;
+		boolean b = false;
+		if(i>10) {
+			b = true;
+		}
+		
+	}
 
 }
---
+++ src/p3/P3_main.java
@@ -0,0 +1,22 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		int i =2;
+		int j = 12;
+		int k = 9;
+		if(i<j) {
+			System.out.print("A");
+		}
+		if(i<k) {
+			System.out.print("B");
+		}
+		if(j<k) {
+			System.out.print("C");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
