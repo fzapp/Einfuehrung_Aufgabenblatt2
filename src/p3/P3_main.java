d3bc7ef9e5120725ddcde4633eb9392af54698de
fzapp <fzapp@smail.uni-loeln.de>
2025-10-29 13:00:08 +0100

    aenderung_Einfuehrung_Aufgabenblatt2_Aufg3_testsHinzugefuegt

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -3,9 +3,9 @@ package p3;
 public class P3_main {
 
 	public static void main(String[] args) {
-		int i =2;
-		int j = 12;
-		int k = 9;
+		int i = 1;
+		int j = 1;
+		int k = 1;
 		if(i<j) {
 			System.out.print("A");
 		}
@@ -20,3 +20,12 @@ public class P3_main {
 	}
 
 }
+
+ //i<j  i<k  j<k erwartet:ABC Test(i=1,j=2,k=3) korrekt
+ //i>=j i<k  j<k erwartet:BC Test(i=1,j=1,k=2) korrekt
+ //i<j  i>=k j<k erwartet:AC Test(nicht mathematisch möglich)
+ //i<j  i<k  j>=k erwartet:AB Test(i=1,j=2,k=2) korrekt
+ //i>=j i>=k j<k erwartet:C Test(i=2,j=1,k=2) korrekt
+ //i>=j i<k  j>=k erwartet:B Test(nicht mathematisch möglich)
+ //i<j  i>=k j>=k erwartet:A Test(i=1,j=2,k=1) korrekt
+ //i>=j i>=k j>=k erwartet:- Test(i=1,j=1,k=1)
