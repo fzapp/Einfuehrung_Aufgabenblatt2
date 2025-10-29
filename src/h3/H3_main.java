41c707ca7fdf3ba78c98b893fa14edb37a550c75
fzapp <fzapp@smail.uni-loeln.de>
2025-10-29 13:40:24 +0100

    aenderung_Einfuehrung_Aufgabenblatt2_HausAufg3_Fertig

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,6 +3,14 @@ package h3;
 public class H3_main {
 
 	public static void main(String[] args) {
+		int i = 3;
+		int j = 1;
+		int k = 5;
+		
+		if(i>j) {
+			
+		}
+		
 		// TODO Auto-generated method stub
 
 	}
